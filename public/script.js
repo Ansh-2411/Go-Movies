6a905fd74c18480c11afd9fdac835373e55391bf
Ansh-2411 <anshsardhara24@gmail.com>
2023-06-07 14:53:35 +0530

    ansh

---
+++ public/script.js
@@ -0,0 +1,76 @@
+// Api for movies
+
+// const options = {
+//     method: 'GET',
+//     headers: {
+//       accept: 'application/json',
+//       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MDM2NTQxYTE5M2FiYTdhMzEyMzEyN2U0YmUyNjhmNiIsInN1YiI6IjY0N2YzY2U0MGZiMzk4MDBjMTI5YzMxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pYG5wJ5Yph8CByPKGWTTIkPPBoMLYxHXKNA5vrfj5-I'
+//     }
+//   };
+  
+//   fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options)
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+//     .catch(err => console.error(err));
+
+
+
+// Api for anime
+
+// const url = 'https://animes5.p.rapidapi.com/';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'ea2740d60bmshdcb4413cfaae6d7p1eb327jsn8651aec1c95e',
+// 		'X-RapidAPI-Host': 'animes5.p.rapidapi.com'
+// 	}
+// };
+
+//   fetch(url, options)
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+//     .catch(err => console.error(err));
+
+
+
+
+// OTT details
+
+// const url = 'https://ott-details.p.rapidapi.com/advancedsearch?start_year=1970&end_year=2020&min_imdb=6&max_imdb=7.8&genre=action&language=english&type=movie&sort=latest&page=1';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'a5b1544b89msh9c68e6ab4fadb9dp1eae0ajsn5eb3eed16905',
+// 		'X-RapidAPI-Host': 'ott-details.p.rapidapi.com'
+// 	}
+// };
+
+//   fetch(url, options)
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+//     .catch(err => console.error(err));
+
+
+
+// Netflix Value Error
+
+// const url = 'https://netflix-original-series-top-100-ranked.p.rapidapi.com/uMEJkR/series';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'content-type': 'application/json',
+// 		'X-RapidAPI-Key': 'ea2740d60bmshdcb4413cfaae6d7p1eb327jsn8651aec1c95e',
+// 		'X-RapidAPI-Host': 'netflix-original-series-top-100-ranked.p.rapidapi.com'
+// 	},
+// 	body: {
+// 		key1: 'value',
+// 		key2: 'value'
+// 	}
+// };
+//   fetch(url,options)
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+//     .catch(err => console.error(err));
+
+
+
